e285b792564f981e8b81eaa4449760a8d7f8a845
Heather1337 <heatherlynn1337@gmail.com>
2020-10-20 16:43:23 -0700

    Completed functions in functions.js

--- functions.js
+++ functions.js
@@ -3,14 +3,44 @@
 
 // 1. isHometown
 
-// Define your function here
+const isHometown = (town) => {
+    if (town === 'San Francisco') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//console.log(isHometown('Los Angeles'))
 
 
 // 2. getFullName
 
-// Define your function here
+const getFullName = (firstName, lastName) => {
+    return `${firstName} ${lastName}`;
+}
+// console.log(getFullName('Heather', 'Leet'))
 
 
 // 3. calculateTotal
 
-// Define your function here
+const calculateTotal = (basePrice, state, tax=0.05) => {
+    let subtotal = basePrice * (1 + tax);
+    let fee = 0;
+
+    if (state === 'CA') {
+        fee = 0.03 * subtotal;
+    } else if (state === 'PA') {
+        fee = 2;
+    } else if (state === 'MA') {
+        if (basePrice <= 100) {
+            fee = 1;
+        } else {
+            fee = 3;
+        }
+    }
+
+    return subtotal + fee;
+}
+
+// console.log(calculateTotal(100, 'MA'))
