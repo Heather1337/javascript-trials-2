bbccca491f6a84d01ed506b83e851a7a0fa35e35
Heather1337 <heatherlynn1337@gmail.com>
2020-10-20 16:33:35 -0700

    Adding solution for arrays.js

--- arrays.js
+++ arrays.js
@@ -4,16 +4,33 @@
 // 1. printIndices
 function printIndices(items) {
   // Replace this with your code
+  for(const i in items) {
+    console.log(i);
+  }
 }
 
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  for (let i = 0; i < items.length; i+=2) {
+    console.log(items[i]);
+  }
 }
+
 
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+  items.sort(function(a, b){
+    return a - b;
+  });
+  let i = 0;
+  const answer = [];
+  while(i < n) {
+    answer.push(items[i]);
+    i++;
+  }
+
+  return answer;
 }
+
