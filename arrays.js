f99244918032459c5a755d7334b26cf5275fb846
vagrant <vagrant@vagrant.vm>
2020-10-20 21:17:43 +0000

    Initial Commit.

---
+++ arrays.js
@@ -0,0 +1,19 @@
+"use strict";
+
+
+// 1. printIndices
+function printIndices(items) {
+  // Replace this with your code
+}
+
+
+// 2. everyOtherItem
+function everyOtherItem(items) {
+  // Replace this with your code
+}
+
+
+// 3. smallestNItems
+function smallestNItems(items, n) {
+  // Replace this with your code
+}
