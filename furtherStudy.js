b42376a87c17ce6668a0d5d22836dfff0980ef5c
Heather1337 <heatherlynn1337@gmail.com>
2020-10-20 19:49:56 -0700

    Adding solution to the furtherstudy.js file.

--- furtherStudy.js
+++ furtherStudy.js
@@ -2,10 +2,47 @@
 
 
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
+  const words1Set = new Set(words1);
+  const words2Set = new Set(words2);
+  const result = [];
+
+  for(const word of words1Set) {
+    if(words2Set.has(word)) {
+      result.push(word);
+    }
+  }
+
+  return result;
 }
+// console.log(wordsInCommon(['cat', 'dog', 'mouse'], ['cat', 'mouse', 'deer']))
 
 
 function kidsGame(names) {
-  // Replace this with your code
+
+  const firstLetterToWords = {};
+  const output = [names.shift()];
+
+  for (const name of names) {
+    if (!(name[0] in firstLetterToWords)) {
+      firstLetterToWords[name[0]] = [name];
+    } else {
+      firstLetterToWords[name[0]].push(name);
+    }
+  }
+
+  while (true) {
+    const len = output.length - 1;
+    const len2 = output[len].length - 1;
+    const startLetter = output[len][len2];
+
+    if (firstLetterToWords[startLetter].length === 0) {
+      break;
+    }
+    const word = firstLetterToWords[startLetter].shift();
+    output.push(word);
+  }
+
+  return output;
 }
+
+// console.log(kidsGame(["bagon", "baltoy", "yamask", "starly", "nosepass", "kalob", "nicky", "booger"]))
